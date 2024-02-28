75491f32812f6095799a232c6874a70a613b784c
yogesh185 <109078572+yogesh185@users.noreply.github.com>
2024-02-28 16:29:41 +0530

    Add files via upload

---
+++ coaching.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    // 1 C++ "Hello, World!" Program
+    // cout<<"Hello World!!"<<endl;
+
+    // 2 C++ Program to Print Number Entered by User
+    // int num;
+    // cin>>num;
+    // cout<<"Your number is "<<num<<endl;
+
+    // 3 C++ Program to Add Two Numbers
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<a+b;
+
+    // 4 C++ Program to Find Quotient and Remainder
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<"Remainder = "<<a%b<<" & Quotient = "<<a/b;
+
+    // 5 C++ Program to Find Size of int, float, double and char in Your System
+    // cout<<"Size of Integer = "<<sizeof(int)<<endl<<"Size of Float = "<<sizeof(float)<<endl<<"Size of Double = "<<sizeof(double)<<endl<<"Size of Char = "<<sizeof(char)<<endl;
+
+    // 6 C++ Program to Swap Two Numbers
+    // int a=5,b=10,c;
+    // c=a;
+    // a=b;
+    // b=c;
+    // cout<<"After Swapping a = "<<a<<" & b = "<<b;
+
+    // 7 C++ Program to Check Whether Number is Even or Odd
+    // int num;
+    // cin>>num;
+    // if (num%2==0)
+    // {
+    //    cout<<"Number is Even.";
+    // }
+    // else cout<<"Number is Odd";
+
+    // 8 C++ Program to Check Whether a character is Vowel or Consonant.
+    // char input;
+    // cin>>input;
+    // if (input=='A'||'E'||'I'||'O'||'U'||'a'||'e'||'i'||'o'||'u')
+    // {
+    //     cout<<"Char is Vowel";
+    // }
+    // else cout<<"Char is Consonant";
+    
+    // 9 C++ Program to Find Largest Number Among Three Numbers
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // if (a>b && a>c)
+    // {
+    //     cout<<"A is Largest";
+    // }
+    // else if (b>a && b>c)
+    // {
+    //     cout<<"B is Largest";
+    // }
+    // else cout<<"C is Largest";
+    
+    // 10 C++ Program to Find All Roots of a Quadratic Equation
+    // float a,b,c,x1,x2,discriminant;
+    // cout<<"Enter the coefficients of a, b and c: "<<endl;
+    // cin>>a>>b>>c;
+    // discriminant=b*b-4*a*c;
+    // if(discriminant>0){
+    //     x1=(-b+sqrt(discriminant))/2*a;
+    //     x2=(-b-sqrt(discriminant))/2*a;
+    //     cout<<"Roots are real and different."<<endl;
+    //     cout<<"x1 = "<<x1<<" & x2 = "<<x2<<endl;
+    // }
+    // else if(discriminant==0){
+    //     x1=-b/(2*a);
+    //     cout<<"Roots are real and equal."<<endl;
+    //     cout<<"x1 = x2 = "<<x1<<endl;
+    // }
+    // else cout<<"Roots are imaginary.";
+
+    // 11 C++ Program to Calculate Sum of Natural Numbers
+    // int n,sum=0;
+    // cout<<"Enter the value of n : "<<endl;
+    // cin>>n;
+    // for ( int i = 1; i <= n; i++)
+    // {
+    //     sum+=i;
+    // }
+    // cout<<"Sum = "<<sum;
+    
+
+    // 12 C++ Program to Check Leap Year
+    // int n;
+    // cout<<"Enter the Year"<<endl;
+    // cin>>n;
+    // if (n%4==0){
+    //     cout<<n<<" is a Leap Year."<<endl;
+    // }
+    // else{
+    //     cout<<"It is not a leap year."<<endl;
+    // }
+        
+
+    // 13 C++ Program to Find Factorial
+    // int n,factorial=1;
+    // cout<<"Enter the number"<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     factorial=factorial*i;
+    // }
+    // cout<<factorial;
+    
+
+    // 14 C++ Program to Generate Multiplication Table
+    // int n;
+    // cout<<"Enter the number"<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+    // }
+    
+
+    // 15 C++ Program to Display Fibonacci Series
+    // int n,t1=0,t2=1,nextTerm=0;
+    // cout<<"Enter the number"<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++){
+    //     if (i==1){
+    //         cout<<t1<<", ";
+    //         continue;
+    //     }
+    //     if (i==2)
+    //     {
+    //         cout<<t2<<", ";
+    //         continue;
+    //     }
+    //     nextTerm=t1+t2;
+    //     t1=t2;
+    //     t2=nextTerm;
+    //     cout<<nextTerm<<", ";
+    // }
+    
+    return 0;
+
+}
